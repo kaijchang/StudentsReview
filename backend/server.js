af49c8f21f76c95de5e45b5a83a8a109fb9ffe65
Kai Chang <kaijchang@gmail.com>
2018-12-19 20:40:27 -0800

    try to get https working

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,39 @@
-const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
-const isDev = Boolean(process.env.dev);
+const express = require('express');
+const https = require('https');
+
+const homedir = require('os').homedir();
+const isDev = process.env.NODE_ENV === 'development';
 
 const port = isDev ? 8000 : 80;
-const app = express();
 
-if (!isDev) {
-    app.use(
-        express.static(
-            path.join(__dirname, '..', 'frontend', 'build')
-        )
-    );
+function register(app) {
+    if (!isDev) {
+        app.use(
+            express.static(
+                path.join(__dirname, '..', 'frontend', 'build')
+            )
+        );
+    }
+
+    return app;
 }
 
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+const http_server = express();
+register(http_server);
+http_server.listen(port, () => {
+    console.log(`http listening on port ${port}`);
 });
+
+if (!isDev) {
+    const options = {
+        key: fs.readFileSync(path.join(homedir, '.acme.sh/studentsreview.me/studentsreview.me.key')),
+        cert: fs.readFileSync(path.join(homedir, '.acme.sh/studentsreview.me/studentsreview.me.cer'))
+    };
+    const https_server = https.createServer(options, register(express()));
+    https_server.listen(443, () => {
+       console.log('https listening on port 443');
+    });
+}
