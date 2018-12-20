ce1eb2d70963eac8777f80e07ce3a32bb7c0282a
Kai Chang <kaijchang@gmail.com>
2018-12-19 20:57:45 -0800

    redirect http to https in production

--- backend/server.js
+++ backend/server.js
@@ -8,34 +8,33 @@ const isDev = process.env.NODE_ENV === 'development';
 
 const port = isDev ? 8000 : 80;
 
-function register(app) {
-    if (!isDev) {
-        app.use(
-            express.static(
-                path.join(__dirname, '..', 'frontend', 'build')
-            )
-        );
-    }
+const http_server = express();
 
-    return app;
-}
-
-const http = express();
-register(http);
-http.listen(port, () => {
-    console.log(`http listening on port ${port}`);
-});
-
-if (!isDev) {
+if (isDev) {
+    // nothing for now
+} else {
+    http_server.get('*', function(req, res) {
+        res.redirect('https://' + req.headers.host + req.url);
+    });
     const options = {
         key: fs.readFileSync('/home/ec2-user/.acme.sh/studentsreview.me/studentsreview.me.key'),
         cert: fs.readFileSync('/home/ec2-user/.acme.sh/studentsreview.me/studentsreview.me.cer')
     };
-    const https_server = https.createServer(
+    let https_server = express();
+    https_server.use(
+        express.static(
+            path.join(__dirname, '..', 'frontend', 'build')
+        )
+    );
+    https_server = https.createServer(
         options,
-        register(express())
+        https_server
     );
     https_server.listen(443, () => {
-       console.log('https listening on port 443');
+        console.log('https listening on port 443');
     });
 }
+
+http_server.listen(port, () => {
+    console.log(`http listening on port ${port}`);
+});
